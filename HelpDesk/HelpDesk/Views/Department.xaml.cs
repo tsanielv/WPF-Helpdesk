bada99c6040d2744c2a0aa1373d31c4d2b8a5396
khoirudi16 <khoirudi16@gmail.com>
2018-08-30 16:56:37 +0700

    OKE

---
+++ HelpDesk/HelpDesk/Views/Department.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelpDesk.Views
+{
+    /// <summary>
+    /// Interaction logic for Department.xaml
+    /// </summary>
+    public partial class Department : Window
+    {
+        public Department()
+        {
+            InitializeComponent();
+        }
+
+        private void button_exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
