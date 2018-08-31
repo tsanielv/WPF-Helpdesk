73724dadf2efaded92312eacf08eec320d7b4a59
Tsani Elvia Nita <elvianitatsani@yahoo.co.id>
2018-08-31 14:46:34 +0700

    oke2

---
+++ HelpDesk/HelpDesk/Controller/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpDesk.Model;
+
+namespace HelpDesk.Controller
+{
+    class LoginController
+    {
+        helpdeskEntities1 db = new helpdeskEntities1();
+
+        public bool cekLogin(string username, string password)
+        {
+            bool cek = false;
+            var temp = db.Users.FirstOrDefault();
+            try
+            {
+                if (temp.Username == username && temp.Password == password)
+                {
+                    cek = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                cek = false;
+                ex.GetBaseException();
+            }
+            return cek;
+        }
+    }
+}
---
+++ HelpDesk/HelpDesk/Views/Login.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HelpDesk.Controller;
+using HelpDesk.Views;
+
+
+namespace HelpDesk.Views
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+
+        LoginController l = new LoginController();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login1_Click(object sender, RoutedEventArgs e)
+        {
+            if (l.cekLogin(txtUsername.Text, txtPassword.Text) == true)
+            {
+                MessageBox.Show("Login Success", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Hide();
+                Dashboard vd = new Dashboard();
+                vd.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Username and Password wrong", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtUsername.Clear();
+                txtPassword.Clear();
+                txtUsername.Focus();
+            }
+        }
+    }
+}
