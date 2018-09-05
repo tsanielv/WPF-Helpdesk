4e792aa101f3b4fb2408916d242ccde3fe598146
Tsani Elvia Nita <elvianitatsani@yahoo.co.id>
2018-09-05 12:06:38 +0700

    oke

---
+++ HelpDesk/HelpDesk/ViewModel/LoginVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpDesk.Models;
+
+namespace HelpDesk.ViewModel
+{
+    class LoginVM
+    {
+        helpdeskEntitiesNew db = new helpdeskEntitiesNew();
+
+        public bool cekLogin(string username, string password)
+        {
+            bool cek = false;
+            var temp = db.Users.FirstOrDefault(db => db.Username == username && db.Password == password);
+
+            try
+            {
+                if (temp.Username == username && temp.Password == password)
+                {
+                    cek = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                cek = true;
+                ex.GetBaseException();
+            }
+            return cek;
+        }
+
+
+    }
+}
